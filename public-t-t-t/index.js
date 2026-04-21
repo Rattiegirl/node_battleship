d68b55f2319d040e37364fd655213f6d65906eaf
Rattiegirl <69050325+Rattiegirl@users.noreply.github.com>
2026-04-21 08:59:49 -0700

    Added reset field button, randomized x-or-o moves upon clicking cell in field

--- public-t-t-t/index.js
+++ public-t-t-t/index.js
@@ -6,15 +6,15 @@ const getField = async () => {
     return data
 }
 
-const move = async () => {
+const move = async (thatOne) => {
     const response = await fetch("/api/move", {
-        method:"POST",
+        method: "POST",
         body: JSON.stringify({
-            player: "O",
-            coordinate: Math.floor(Math.random()*9)
+            player: Math.random() > 0.5 ? "O" : "X",
+            coordinate: thatOne
         }),
         headers: {
-            "Content-Type":"application/json"
+            "Content-Type": "application/json"
         }
     })
     const data = await response.json()
@@ -24,30 +24,55 @@ const move = async () => {
 const unveilField = (field) => {
     // найти элементы
     const box = document.querySelector("#field")
-// цикл по каждому field элементу
+    // цикл по каждому field элементу
     let peremennaya = ""
-    for (const dot of field){
-        peremennaya += `<div class="cell">${dot}</div>`
+    for (let i = 0; i < field.length; i++) {
+        const dot = field[i]
+        peremennaya += `<div data-id="${i}" class="cell">${dot}</div>`
     }
-    box.innerHTML = peremennaya 
-// создать ячейку и append в специально подготовленный wrapper (div) 
+    box.innerHTML = peremennaya
+    // создать ячейку и append в специально подготовленный wrapper (div) 
 
-// document.createElement()    document.body.append()
+    // document.createElement()    document.body.append()
 
-// или
+    // или
 
-// <div>элемент</div>
-// <div>элемент</div>
-// <div>элемент</div>...  innerHTML(wrapper)
+    // <div>элемент</div>
+    // <div>элемент</div>
+    // <div>элемент</div>...  innerHTML(wrapper)
+}
+
+
+const resetField = async () => {
+    const response = await fetch("/api/fields", {
+        method: "PUT"
+    })
+    const data = await response.json()
+    return data
 }
 
 // app
-move().then(()=>{
-    
-    getField().then((data) => {
-        console.log(data)
-        unveilField(data.field)
-    })
-
+getField().then((data) => {
+    console.log(data)
+    unveilField(data.field)
 })
 
+const playingField = document.querySelector("#field")
+playingField.addEventListener("click", async (event) => {
+    console.log(event)
+    const { target } = event
+    if (target.classList.contains("cell")) {
+        const thatOne = target.getAttribute("data-id")
+        const data = await move(thatOne)
+        unveilField(data.field)
+    }
+})
+
+
+
+const resetButton = document.querySelector("#resetBtn")
+resetButton.addEventListener("click", async () => {
+    const { field } = await resetField()
+    unveilField(field)
+    alert("You have just reset the field!")
+})
